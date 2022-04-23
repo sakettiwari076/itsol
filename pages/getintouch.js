cb86d9b28e7a2d4396effb0a5375bf87183802a0
Saket <sakettiwari076@gmail.com>
2022-04-23 23:53:28 +0530

    test

--- pages/getintouch.js
+++ pages/getintouch.js
@@ -3,17 +3,17 @@ import React from 'react'
 const getintouch = () => {
   return (
     <div className='bgtt pt-14  font-bold pad2 '>
-    <h1 className='text-6xl  cen font-mono text-black font-mono'>GET IN TOUCH</h1>
+    <h1 className='text-6xl  cen  text-black font-mono'>GET IN TOUCH</h1>
     <div className="text-gray-600 body-font relative">
     <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-      <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
         <iframe width="100%" height="100%" className="absolute inset-0  " frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" ></iframe>
-        <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-          <div className="lg:w-1/2 px-6">
-            <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-            <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
-          </div>
-          <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+           <div className="lg:w-1/2 px-6">
+           <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+           <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+            </div>
+           <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
             <a className="text-indigo-500 leading-relaxed">example@email.com</a>
             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
